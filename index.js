cead5e1ee1a271f0525a7eb4c6eb358cde2450c1
Ashraful-IR <rifo0118@gmail.com>
2026-07-11 02:49:02 +0600

    Database setup done for the backend.

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4","0.0.0.0","1.1.1.1","103.129.238.255","192.168.1.1"]);
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+const uri = process.env.DB_URL;
+const port = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
